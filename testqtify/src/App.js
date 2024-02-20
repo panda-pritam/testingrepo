e80e05e720f48bb5ba1b0aa496f163b7101ab8ec
panda-pritam <mondalrumki02@gmail.com>
2024-02-21 00:38:11 +0530

    hero sections has made generic

--- testqtify/src/App.js
+++ testqtify/src/App.js
@@ -2,14 +2,20 @@ import logo from "./logo.svg";
 // import './App.css';
 import Navbar from "./components/Navbar/navBar";
 import HeroSection from "./components/hero/HeroSection";
-
+import heroImg from "./assets/hero_headphones.png";
 function App() {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      <HeroSection
+        textLine1={"100 Thousand Songs, ad-free"}
+        textLine2={"Over thousands podcast episodes"}
+        image={heroImg}
+      />
     </div>
   );
 }
 
 export default App;
+
+//{textLine1,textLine2,image}
